51e0d1b4af6feecc76967671af24b5e8e7e1663f
TiagoDM <tiago.motasaj@gmail.com>
2023-01-17 00:15:20 -0300

    form binding learned

--- src/main/java/saj/mota/tiago/sbformexample/controllers/FormController.java
+++ src/main/java/saj/mota/tiago/sbformexample/controllers/FormController.java
@@ -4,20 +4,20 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import saj.mota.tiago.sbformexample.entities.Name;
+
 @Controller
 @RequestMapping("/form")
 public class FormController {
 
     @GetMapping("/fill")
-    public String fill() {
+    public String fill(Model model) {
+        model.addAttribute("name", new Name());
         return "form";
     }
 
     @PostMapping("/submit")
-    public String submit(@RequestBody String name, Model model) {
-        if (name.isEmpty()) {
-            name = "empty input";
-        }
+    public String submit(@ModelAttribute Name name, Model model) {
         model.addAttribute("name", name);
         return "list";
     }
